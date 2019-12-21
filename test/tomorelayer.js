0b733cc74f62ab131fad35bd4671a2da3aea4149
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-12-21 08:21:10 +0000

    add tomorelayer

---
+++ test/tomorelayer.js
@@ -0,0 +1,27 @@
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let should = chai.should()
+let expect = chai.expect
+let config = require('config')
+let urljoin = require('url-join')
+let uri = (config.tomorelayer || {}).uri
+
+if (!uri) {
+    return
+}
+
+chai.use(chaiHttp)
+describe('TomoRelayer', () => {
+	describe('/GET site', () => {
+		it('it should GET site', (done) => {
+            let url = uri
+			chai.request(url)
+				.get('/')
+				.end((err, res) => {
+					res.should.have.status(200)
+					res.should.be.html
+					done()
+				})
+		})
+	})
+})
