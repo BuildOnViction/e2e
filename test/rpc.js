3761e2c5eeefcee38c0966774e4f7fb4b0a4d0ad
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-04-16 14:59:02 +0000

    add test more rpc

--- test/rpc.js
+++ test/rpc.js
@@ -4,42 +4,44 @@ let should = chai.should()
 let expect = chai.expect
 let config = require('config')
 let urljoin = require('url-join')
-let uri = (config.rpc || {}).uri
+let rpc = (config.rpc || {})
 
 chai.use(chaiHttp)
 describe('TomoChain RPC', () => {
-    if (!uri) {
+    if (!rpc) {
         return
     }
 
-    describe('/POST gasPrice', () => {
-        it('it should get gasPrice', (done) => {
-            let url = uri
-            chai.request(url)
-                .post('/')
-                .send({ "jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":73 })
-                .end((err, res) => {
-                    res.should.have.status(200)
-                    res.should.be.json
-                    done()
-                })
+    rpc.forEach(uri => {
+        describe(`/POST gasPrice ${uri}`, () => {
+            it(`it should get gasPrice ${uri}`, (done) => {
+                let url = uri
+                chai.request(url)
+                    .post('/')
+                    .send({ "jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":73 })
+                    .end((err, res) => {
+                        res.should.have.status(200)
+                        res.should.be.json
+                        done()
+                    })
+            })
         })
-    })
 
-    describe('/POST getBlock', () => {
-        it('it should get getBlock', (done) => {
-            let url = uri
-            chai.request(url)
-                .post('/')
-                .send({ "jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false],"id":1 })
-                .end((err, res) => {
-                    res.should.have.status(200)
-                    res.should.be.json
-                    let timestamp = parseInt(res.body.result.timestamp, 16)
-                    let delta = Math.floor(Date.now() / 1000) - timestamp
-                    expect(delta).to.below(120)
-                    done()
-                })
+        describe(`/POST getBlock ${uri}`, () => {
+            it(`it should get getBlock ${uri}`, (done) => {
+                let url = uri
+                chai.request(url)
+                    .post('/')
+                    .send({ "jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false],"id":1 })
+                    .end((err, res) => {
+                        res.should.have.status(200)
+                        res.should.be.json
+                        let timestamp = parseInt(res.body.result.timestamp, 16)
+                        let delta = Math.floor(Date.now() / 1000) - timestamp
+                        expect(delta).to.below(120)
+                        done()
+                    })
+            })
         })
     })
 
