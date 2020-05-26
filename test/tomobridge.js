d81b1fc84e4c258717cce75479a5ba174686fba0
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-05-26 08:07:50 +0000

    check fee balance

--- test/tomobridge.js
+++ test/tomobridge.js
@@ -5,6 +5,7 @@ let expect = chai.expect
 let config = require('config')
 let urljoin = require('url-join')
 let uri = (config.tomobridge || {}).uri
+let BigNumber = require('bignumber.js')
 
 chai.use(chaiHttp)
 describe('TomoBridge', () => {
@@ -27,4 +28,24 @@ describe('TomoBridge', () => {
         })
     })
 
+    describe('/GET eth fee balance', () => {
+        it('it should have enough balance for the tx fee', (done) => {
+            let address = config.tomobridge.usdtFeeWallet
+            if (!address) {
+                return done()
+            }
+            let apiKey = process.env.ETHERSCAN_APIKEY || config.etherscanApiKey
+            let url = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`
+            chai.request(url)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.should.be.json
+                    let balance = parseFloat((new BigNumber(res.body.result)).dividedBy(1e18).toString(10))
+                    console.log(`address=${address} balance=${balance}`)
+                    expect(balance).to.above(0.05, 'Not enough balance for wallet fee')
+                    done()
+                })
+        })
+    })
 })
