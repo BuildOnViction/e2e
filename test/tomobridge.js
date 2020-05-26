00bf448a120bc5987a5704f5276823930cb20dbd
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-05-26 08:56:17 +0000

    add check total supply and locked balance

--- test/tomobridge.js
+++ test/tomobridge.js
@@ -6,6 +6,7 @@ let config = require('config')
 let urljoin = require('url-join')
 let uri = (config.tomobridge || {}).uri
 let BigNumber = require('bignumber.js')
+let TomoJS = require('tomojs')
 
 chai.use(chaiHttp)
 describe('TomoBridge', () => {
@@ -46,6 +47,106 @@ describe('TomoBridge', () => {
                     expect(balance).to.above(0.05, 'Not enough balance for wallet fee')
                     done()
                 })
+        })
+    })
+
+    describe('/GET locked balance and total supply', () => {
+        it('it should have enough locked', (done) => {
+            let map = []
+            let btc = config.tomobridge.lockedBTC
+            let apiKey = process.env.ETHERSCAN_APIKEY || config.etherscanApiKey
+
+            if (btc) {
+                let url = `https://blockchain.info/q/addressbalance/${btc}`
+                let p = new Promise((resolve, reject) =>  {
+                    return chai.request(url)
+                        .get('/')
+                        .end((err, res) => {
+                            res.should.have.status(200)
+                            let balance = parseFloat((new BigNumber(res.text)).dividedBy(1e8).toString(10))
+                            console.log(`token=BTC address=${btc} balance=${balance}`)
+                            resolve()
+                        })
+                })
+                map.push(p)
+
+            }
+
+            let eth = config.tomobridge.lockedETH
+            if (eth) {
+                let url = `https://api.etherscan.io/api?module=account&action=balance&address=${eth}&tag=latest&apikey=${apiKey}`
+                let p = new Promise((resolve, reject) =>  {
+                    return chai.request(url)
+                        .get('/')
+                        .end((err, res) => {
+                            res.should.have.status(200)
+                            res.should.be.json
+                            let balance = parseFloat((new BigNumber(res.body.result)).dividedBy(1e18).toString(10))
+                            console.log(`token=ETH address=${eth} balance=${balance}`)
+                            resolve()
+                        })
+                })
+                map.push(p)
+            }
+
+            let usdt = config.tomobridge.lockedUSDT
+            if (usdt) {
+                let url = `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdac17f958d2ee523a2206206994597c13d831ec7&address=${usdt}&tag=latest&apikey=${apiKey}`
+                let p = new Promise((resolve, reject) =>  {
+                    return chai.request(url)
+                        .get('/')
+                        .end((err, res) => {
+                            res.should.have.status(200)
+                            res.should.be.json
+                            let balance = parseFloat((new BigNumber(res.body.result)).dividedBy(1e6).toString(10))
+                            console.log(`token=USDT address=${eth} balance=${balance}`)
+                            resolve()
+                        })
+                })
+                map.push(p)
+            }
+
+            let tomojs = new TomoJS('https://rpc.tomochain.com')
+            let trc21usdt = config.tomobridge.trc21USDT
+
+            if (trc21usdt) {
+                let p = new Promise((resolve, reject) =>  {
+                    return tomojs.tomoz.getTokenInformation(trc21usdt).then(data => {
+                        let totalSupply = data.totalSupply
+                        console.log(`token=USDT address=${trc21usdt} totalSupply=${totalSupply}`)
+                        resolve()
+                    }).catch(e => reject(e))
+                })
+                map.push(p)
+            }
+
+            let trc21eth = config.tomobridge.trc21ETH
+
+            if (trc21eth) {
+                let p = new Promise((resolve, reject) =>  {
+                    return tomojs.tomoz.getTokenInformation(trc21eth).then(data => {
+                        let totalSupply = data.totalSupply
+                        console.log(`token=ETH address=${trc21eth} totalSupply=${totalSupply}`)
+                        resolve()
+                    }).catch(e => reject(e))
+                })
+                map.push(p)
+            }
+
+            let trc21btc = config.tomobridge.trc21BTC
+
+            if (trc21btc) {
+                let p = new Promise((resolve, reject) =>  {
+                    return tomojs.tomoz.getTokenInformation(trc21btc).then(data => {
+                        let totalSupply = data.totalSupply
+                        console.log(`token=BTC address=${trc21btc} totalSupply=${totalSupply}`)
+                        resolve()
+                    }).catch(e => reject(e))
+                })
+                map.push(p)
+            }
+            
+            Promise.all(map).then(() => done()).catch(() => done())
         })
     })
 })
