d098f2cf6dccce0d6027a96ab8cba6e589f8cd0c
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-06-03 09:12:40 +0000

    add monitor product status

--- test/index.js
+++ test/index.js
@@ -1,10 +1,22 @@
 const request = require('request')
 const urljoin = require('url-join')
+const Stats = require('../stats/product')
 
 afterEach(async function() {
+
+    let product = path.basename(this.currentTest.file, '.js').toUpperCase()
+    let env = (process.env.NODE_ENV || 'empty').toUpperCase()
+    await Stats.push({
+        table: 'statuses',
+        product: product,
+        env: env,
+        status: this.currentTest.state,
+        value: 1
+    })
+
     if (this.currentTest.state === 'failed') {
         let slackUri = process.env.SLACK_URI
-        let p = new Promise(async (resolve, reject) => {
+        let p = new Promise((resolve, reject) => {
             let url = urljoin(slackUri)
             let data = {
                 blocks: [
@@ -15,6 +27,7 @@ afterEach(async function() {
                             text: `
 \`\`\`
 Environment: ${(process.env.NODE_ENV || '').toUpperCase()}
+Product: ${product}
 Title: ${this.currentTest.title}
 Error Message: ${this.currentTest.err.message}
 Test File: ${this.currentTest.file}
