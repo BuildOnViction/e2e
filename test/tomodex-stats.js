c23c7f6696e1aeaf96c3b6d927f3f3cdf32e7357
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-10-27 04:54:32 +0000

    fix test dex

--- test/tomodex-stats.js
+++ test/tomodex-stats.js
@@ -319,8 +319,8 @@ describe('TomoDEX-Stats', () => {
                     env: process.env.NODE_ENV,
                     value: allTotal
                 })
-            })
-            done()
+                done()
+            }).catch(() => done())
         })
     })
 
